93e884900877638f586117fb48a9cbcba1277ced
Devendra <96000752+sumitastrologer@users.noreply.github.com>
2022-11-26 19:22:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function aries () {
+    document.getElementById("hidden-aries").style.display = 'block';
+}
+function taurus () {
+    document.getElementById("hidden-taurus").style.display = 'block';
+}
+function gemini () {
+    document.getElementById("hidden-gemini").style.display = 'block';
+}
+function cancer () {
+    document.getElementById("hidden-cancer").style.display = 'block';
+}
+function leo () {
+    document.getElementById("hidden-leo").style.display = 'block';
+}
+function virgo () {
+    document.getElementById("hidden-virgo").style.display = 'block';
+}
+function libra () {
+    document.getElementById("hidden-libra").style.display = 'block';
+}
+function scorpio () {
+    document.getElementById("hidden-scorpio").style.display = 'block';
+}
+function sagittarius () {
+    document.getElementById("hidden-sagittarius").style.display = 'block';
+}
